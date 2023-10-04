cf41f6924678518993ca42d16584e6ea00ea9b52
Pedro Hernández <phernandez@ingelabs.com>
2023-10-04 12:45:29 +0200

    fixup: feedback

--- src/malva/java/math/BigDecimalTest.java
+++ src/malva/java/math/BigDecimalTest.java
@@ -5,20 +5,13 @@ import java.math.BigDecimal;
 import malva.TestCase;
 
 public class BigDecimalTest extends TestCase {
-  public static void testStrippingZeros() {
-    // Array as { test, result }
-    final BigDecimal[][] testCases = {
-      {new BigDecimal("0.1234000"), new BigDecimal("0.1234")},
-      {new BigDecimal("0e2"), BigDecimal.ZERO},
-      {new BigDecimal("100000"), new BigDecimal("1e5")}
-    };
-
-    for (BigDecimal[] testCase : testCases) {
-      assertEquals(testCase[1], testCase[0].stripTrailingZeros());
-    }
+  public static void testStripTrailingZeros() {
+    assertEquals(new BigDecimal("0.1234"), new BigDecimal("0.1234000").stripTrailingZeros());
+    assertEquals(BigDecimal.ZERO, new BigDecimal("0e2").stripTrailingZeros());
+    assertEquals(new BigDecimal("1e5"), new BigDecimal("100000").stripTrailingZeros());
   }
 
   public static void main(String[] args) {
-    testStrippingZeros();
+    testStripTrailingZeros();
   }
 }
