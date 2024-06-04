fece1f0694269e27d1c9bc00d3878ebf8201c7f9
Guillermo Rodríguez <guille.rodriguez@gmail.com>
2024-06-04 17:53:35 +0200

    New test for DateFormatSymbols

---
+++ src/malva/java/text/DateFormatSymbolsTest.java
@@ -0,0 +1,20 @@
+package malva.java.text;
+
+import java.text.DateFormatSymbols;
+import java.util.Locale;
+
+import malva.TestCase;
+
+public class DateFormatSymbolsTest extends TestCase {
+    public static void testSymbols() {
+        DateFormatSymbols dfs = new DateFormatSymbols(Locale.US);
+
+        String[] amPm = dfs.getAmPmStrings();
+        assertEquals("AM", amPm[0]);
+        assertEquals("PM", amPm[1]);
+
+        String[] eras = dfs.getEras();
+        assertEquals("BC", eras[0]);
+        assertEquals("AD", eras[1]);
+    }
+}
