8d0efc4437607823617b3d01bf20d8430478a9ba
Guillermo Rodríguez <guille.rodriguez@gmail.com>
2024-06-05 10:45:16 +0200

    fixup! Test was not enabled

--- src/malva/java/text/DateFormatSymbolsTest.java
+++ src/malva/java/text/DateFormatSymbolsTest.java
@@ -6,15 +6,19 @@ import java.util.Locale;
 import malva.TestCase;
 
 public class DateFormatSymbolsTest extends TestCase {
-    public static void testSymbols() {
-        DateFormatSymbols dfs = new DateFormatSymbols(Locale.US);
+  public static void testParseSymbols() {
+    DateFormatSymbols dfs = new DateFormatSymbols(Locale.US);
 
-        String[] amPm = dfs.getAmPmStrings();
-        assertEquals("AM", amPm[0]);
-        assertEquals("PM", amPm[1]);
+    String[] amPm = dfs.getAmPmStrings();
+    assertEquals("AM", amPm[0]);
+    assertEquals("PM", amPm[1]);
 
-        String[] eras = dfs.getEras();
-        assertEquals("BC", eras[0]);
-        assertEquals("AD", eras[1]);
-    }
+    String[] eras = dfs.getEras();
+    assertEquals("BC", eras[0]);
+    assertEquals("AD", eras[1]);
+  }
+
+  public static void main(String[] args) {
+    testParseSymbols();
+  }    
 }
