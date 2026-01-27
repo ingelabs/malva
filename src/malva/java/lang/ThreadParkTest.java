607142109e96807bc3c760cead4c8ce3e7d45350
Guillermo Rodríguez <grodriguez@ingelabs.com>
2026-01-27 11:12:38 +0100

    Add tests for monotonic sleep/wait
    Add tests to verify that sleep/wait use a monotonic clock. These are
    disabled by default since they require changing the system time, but
    they are still useful for manual tests.

--- src/malva/java/lang/ThreadParkTest.java
+++ src/malva/java/lang/ThreadParkTest.java
@@ -1,13 +1,23 @@
 package malva.java.lang;
 
+import java.io.InputStream;
+import java.io.IOException;
 import java.util.concurrent.locks.LockSupport;
 
 import malva.TestCase;
 
 public class ThreadParkTest extends TestCase {
+    /**
+     * Disabled by default since these tests need to change the system time,
+     * which is OS-specific and requires privileges.
+     */
+    private static final boolean ENABLE_TIME_JUMP_TESTS = false;
+
     private static final long NOMINAL_WAIT_MS = 500;
     private static final long WAIT_TOL_MS = 50;
     private static final long JOIN_EXTRA_MS = 100;
+
+    private static final long NOMINAL_JUMP_TESTS_WAIT_MS = 3000;
 
     private static void sleep(long ms) {
         try { Thread.sleep(ms); } catch (InterruptedException e) {}
@@ -76,9 +86,67 @@ public class ThreadParkTest extends TestCase {
         assertTimeInRangeMs(elapsedHolder[0], NOMINAL_WAIT_MS, WAIT_TOL_MS);
     }
 
-    public static void main(String args[]) throws InterruptedException {
+    private static void setSystemTimeSeconds(long epochSeconds) throws IOException, InterruptedException {
+        String[] cmd = new String[] { "date", "-s", "@" + epochSeconds };
+        Process p = Runtime.getRuntime().exec(cmd);
+        // Read until EOF to make sure command has run
+        // Avoid calling p.waitFor since it uses Object.wait internally in GNU Classpath
+        p.getErrorStream().close();
+        InputStream in = p.getInputStream();
+        while (in.read() != -1) {}
+        in.close();
+    }
+
+    private static void testWaitOrSleepWithTimeJump(final boolean useSleep) throws Exception {
+        final long[] elapsedHolder = new long[1];
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                long t1 = nowNanos();
+                if (useSleep) {
+                    try { Thread.sleep(NOMINAL_JUMP_TESTS_WAIT_MS); } catch (InterruptedException e) {}
+                } else {
+                    Object lock = ThreadParkTest.class;
+                    synchronized (lock) {
+                        try { lock.wait(NOMINAL_JUMP_TESTS_WAIT_MS); } catch (InterruptedException e) {}
+                    }
+                }
+                elapsedHolder[0] = (nowNanos() - t1) / 1000000;
+            }
+        });
+        t.start();
+
+        long nowSeconds = System.currentTimeMillis() / 1000;
+
+        // Move the system time forward, check that the thread didn't exit early
+        setSystemTimeSeconds(nowSeconds + 60);
+        t.join(JOIN_EXTRA_MS);
+        if (!t.isAlive())
+            fail((useSleep ? "sleep" : "wait") + "() finished early when system time jumped forward");
+
+        // Move the system time backward, check that the thread finishes in time
+        setSystemTimeSeconds(nowSeconds - 60);
+        assertThreadFinishes(t, JOIN_EXTRA_MS + NOMINAL_JUMP_TESTS_WAIT_MS);
+        assertTimeInRangeMs(elapsedHolder[0], NOMINAL_JUMP_TESTS_WAIT_MS, WAIT_TOL_MS);
+
+        // Restore system time (approximation)
+        setSystemTimeSeconds(nowSeconds + NOMINAL_JUMP_TESTS_WAIT_MS / 1000);
+    }
+
+    public static void testSleepWithTimeJump() throws Exception {
+        testWaitOrSleepWithTimeJump(true);
+    }
+
+    public static void testWaitWithTimeJump() throws Exception {
+        testWaitOrSleepWithTimeJump(false);
+    }
+
+    public static void main(String args[]) throws Exception {
         testUntimedPark();
         testRelativeTimedPark();
         testAbsoluteTimedPark();
+        if (ENABLE_TIME_JUMP_TESTS) {
+            testSleepWithTimeJump();
+            testWaitWithTimeJump();
+        }
     }
 }
