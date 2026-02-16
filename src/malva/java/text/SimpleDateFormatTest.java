9ffc7469c916d38d02a388ec1a3d985b28bb66c6
Guillermo Rodríguez <grodriguez@ingelabs.com>
2026-02-16 02:19:37 +0100

    extra: sdf with timezone

--- src/malva/java/text/SimpleDateFormatTest.java
+++ src/malva/java/text/SimpleDateFormatTest.java
@@ -1,5 +1,6 @@
 package malva.java.text;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -12,7 +13,21 @@ public class SimpleDateFormatTest extends TestCase {
     assertEquals(new Date(1288166400000L), sdf.parse("2010/10/27 10:00:00 Wed"));
   }
 
+  public static void testParseWithTimeZone() throws Exception {
+    final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US);
+    // No timezone -- should throw   
+    assertThrows(new Block() {
+      @Override
+      public void run() throws Exception {
+        sdf.parse("2009-04-14T20:18:10Z");
+      }
+    }, ParseException.class);
+    // With timezone
+    assertEquals(new Date(1239761890000L), sdf.parse("2009-04-14T20:18:10-0600"));
+  }
+
   public static void main(String[] args) throws Exception {
-//    testParse();
+    testParse();
+    //testParseWithTimeZone();
   }
 }
